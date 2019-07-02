022d1896a763607827e8c3d3b526ce4d9467c186
Dima Gerasimov <karlicoss@gmail.com>
2019-07-02 21:43:15 +0100

    add submodule with test data

--- tests/test_kobo.py
+++ tests/test_kobo.py
@@ -5,7 +5,7 @@ import kobuddy
 # TODO ugh, horrible
 def get_test_dbs():
     # db = Path(__file__).absolute().parent.parent / 'KoboShelfes' / 'KoboReader.sqlite.0'
-    db = Path(__file__).absolute().parent.parent / 'kobo_notes' / 'input' / 'KoboReader.sqlite'
+    db = Path(__file__).absolute().parent / 'data' / 'kobo_notes' / 'input' / 'KoboReader.sqlite'
     return [db]
 kobuddy._get_all_dbs = get_test_dbs
 
