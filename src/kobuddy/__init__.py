11c5cec921ad9b6bf1923badd664381a204f326a
Dima Gerasimov <karlicoss@gmail.com>
2019-07-02 20:34:59 +0100

    Pyscaffold setup

--- src/kobuddy/__init__.py
+++ src/kobuddy/__init__.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 # pip3 install dataset
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = get_distribution(dist_name).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+finally:
+    del get_distribution, DistributionNotFound
+
+
 from datetime import datetime
 import logging
 import os
