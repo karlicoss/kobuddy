1eb7a0bf2d7c794fef26cd459dfc432546fe0fb8
Ulisse Mini <ulisse.mini@gmail.com>
2021-08-17 10:49:20 -0400

    Fix get mountpoint on latest lsblk

--- src/kobuddy/kobo_device.py
+++ src/kobuddy/kobo_device.py
@@ -3,13 +3,16 @@ from pathlib import Path
 import subprocess
 from typing import Optional
 
+def _mountpoint(k):
+    # Different lsblk versions give different json, see kobuddy/issues/15
+    return k.get('mountpoint') or k['mountpoints'][0]
 
 def get_kobo_mountpoint(label: str='KOBOeReader') -> Optional[Path]:
     try:  # Linux
         xxx = subprocess.check_output(['lsblk', '-f', '--json']).decode('utf8')
         jj = json.loads(xxx)
         kobos = [d for d in jj['blockdevices'] if d.get('label', None) == label]
-        kobos = [k['mountpoint'] for k in kobos]
+        kobos = [_mountpoint(k) for k in kobos]
     except FileNotFoundError:  # macOS (does not have lsblk)
         output = subprocess.check_output(('df', '-Hl')).decode('utf8')
         output_parts = [o.split() for o in output.split('\n')]
